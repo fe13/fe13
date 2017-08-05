1f266af97dc3c192d908a8e9c93233d4b7b5da62
Tom Wang <tom.wanghaiyi@gmail.com>
2017-08-05 18:36:17 +0800

    Post api pagination

--- apis/posts.js
+++ apis/posts.js
@@ -4,13 +4,33 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const size = parseInt(req.query.pageSize) || 20;
   const type = req.query.type;
-  const posts = await Post.find(
-    type ? { type } : {},
-    { __v: 0 }, 
-    { sort: { updated: -1, score: 1 }}
-  );
-  res.json(posts);
+  const [count, posts] = await Promise.all([
+    Post.count(),
+    Post.find(
+      type ? { type } : {},
+      { __v: 0 }, 
+      { sort: { updated: -1, score: 1 }}
+    ).skip(page * size - size).limit(size)
+  ]);
+
+  res.json({ 
+    data: posts,
+    pagination: {
+      pageSize: size,
+      currPage: page,
+      nextPage: page * size < count ? page + 1 : null,
+      totalItems: count,
+      totalPages: Math.ceil(count / size)
+    }
+  });
+});
+
+router.put('/:id/upvote', async (req, res, next) => {
+  const post = await Post.findByIdAndUpdate(req.params.id, { $inc: { score: 1 } });
+  res.json({ score: post.score + 1 });
 });
 
 module.exports = router;
